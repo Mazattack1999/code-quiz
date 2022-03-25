b3d6717296fea6efca4f6ea657bb9a3c8b3509a4
micah_z <micah.a.zahn@gmail.com>
2022-03-25 11:43:02 -0500

    add startQuiz, startTimer, and quizHandling functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // initialize global variables
 var time = 75;
+var timer = document.querySelector(".timer");
+var interval;
 var quizInProgress = false; // tell if quiz is in progress
-var main = document.querySelector("main"); // store main
+var quizMain = document.querySelector("main"); // store main
 var quizHeader = document.querySelector(".header"); // store quiz header text
 // questions, their answers, and their answer choices stored in an array of objects
 var questions = [{
@@ -15,8 +17,11 @@ var questions = [{
     answerChoices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
 },
 ]
+var answerSelect = "";
 
 // function definitions
+
+// bring quiz back to start screen
 function resetQuiz(){
     // set quizHeader text to start text
     quizHeader.innerHTML = "Coding Quiz Challenge";
@@ -25,14 +30,63 @@ function resetQuiz(){
     var pText = document.createElement("p");
     pText.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
     pText.classList.add("description");
-    main.appendChild(pText);
+    quizMain.appendChild(pText);
 
     // create start button
     var startBtn = document.createElement("button");
     startBtn.innerHTML = "Start Quiz";
     startBtn.classList.add("start-btn");
-    main.appendChild(startBtn);
-    startBtn.addEventListener("click", startQuiz()); // startBtn event listener
+    quizMain.appendChild(startBtn);
+    startBtn.addEventListener("click", startQuiz); // startBtn event listener
+}
+
+// moving from start screen into quiz screen
+function startQuiz(){
+    // save description paragraph
+    var para = document.querySelector(".description"); 
+    // remove description paragraph
+    quizMain.removeChild(para);
+    // save start button
+    var sBtn = document.querySelector(".start-btn");
+    // remove start button
+    quizMain.removeChild(sBtn);
+    
+    // start the timer
+    startTimer();
+
+    //handle quiz run functionality
+    quizHandling();
+}
+
+// start the timer
+function startTimer() {
+    interval = setInterval(function(){
+        timer.innerHTML = "Time: " + time + "s";
+        time--;
+        
+        // clear interval when time runs out
+        if (time <= 0) {
+            clearInterval(interval);
+        }
+        
+    }, 1000);
+} 
+
+function quizHandling(){
+    // set quiz in progress to true
+    quizInProgress = true;
+
+    // loop through questions array
+    for (var i = 0; i < questions.length; i++){
+        setUpQuestion(i);
+    }
+
+}
+
+function setUpQuestion(questionNum){
+    quizHeader.innerHTML = questions[questionNum].question;
 }
 
 
+// initial startup
+resetQuiz();
